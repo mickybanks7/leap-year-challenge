cd527b74e65f699f5a43aad968e684ae4c9a7ebb
Emeka <mjbanks4real@gmail.com>
2022-02-02 06:45:43 +0000

    added the last logic of both key options for a leap year using the OR logic

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ if year % 4 == 0 and year % 400 == 0:
     print("Leap year")
 elif year % 100 == 0:
     print("Not leap year")
-
+elif year % 400 == 0 or year % 4 == 0:
+    print("Leap year")
+else:
+    print("Not leap year")
 
 
